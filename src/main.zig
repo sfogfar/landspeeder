73e6d0d395bde20ee9e84dcc138d870f427c5ee4
Sam Cooper <003.sjc@gmail.com>
2025-05-17 22:29:54 +0100

    Show green for good, red for not so good

--- src/main.zig
+++ src/main.zig
@@ -2,33 +2,40 @@ const std = @import("std");
 
 const LAMBDA_CHAR = 0x03BB;
 
-pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+// ANSI escape codes
+const BOLD = "\x1b[1m";
+const RED = "\x1b[31m";
+const GREEN = "\x1b[32m";
+const RESET = "\x1b[0m";
 
+pub fn main() !void {
     // TODO: switch to the arena alloc once stable(ish)
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const display_path = try getDisplayPath(alloc);
+    var env_map = try std.process.getEnvMap(alloc);
+    defer env_map.deinit();
+
+    const display_path = try getDisplayPath(alloc, &env_map);
     defer alloc.free(display_path);
 
-    // TODO: use a struct for prompt
+    const status_colour_code = try getStatusColourCode(&env_map);
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
     try stdout.print("{s}\n", .{display_path});
-    try stdout.print("{u} ", .{LAMBDA_CHAR});
+    try stdout.print("{s}{s}{u} ", .{ status_colour_code, BOLD, LAMBDA_CHAR });
 
     try bw.flush();
 }
 
 /// Returns a formatted path.
 /// Caller owns resulting string and should free it when done.
-fn getDisplayPath(alloc: std.mem.Allocator) ![]const u8 {
-    var env_map = try std.process.getEnvMap(alloc);
-    defer env_map.deinit();
-
-    const pwd = env_map.get("PWD") orelse "";
+fn getDisplayPath(alloc: std.mem.Allocator, env_map: *const std.process.EnvMap) ![]const u8 {
+    const pwd = env_map.get("PWD") orelse "unknown";
     const home = env_map.get("HOME") orelse "";
 
     // Attempt to replace full $HOME with ~
@@ -37,4 +44,11 @@ fn getDisplayPath(alloc: std.mem.Allocator) ![]const u8 {
     } else {
         return try alloc.dupe(u8, pwd);
     }
+}
+
+/// Returns the exit code of the last command.
+fn getStatusColourCode(env_map: *const std.process.EnvMap) ![]const u8 {
+    const status_str = env_map.get("LAST_CMD_STATUS") orelse "0";
+    const status_code = try std.fmt.parseInt(u8, status_str, 10);
+    return if (status_code > 0) RED else GREEN;
 }
